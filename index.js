570f5aa58e9023e03e7c1c302282ba0fd8c5ed2b
Priates of Zimsey <realcoder2023@gmail.com>
2025-09-09 23:37:00 -0400

    added threading sleep in js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 
 function main() {
 
-
     const R = Math.floor(Math.random() * 255 / 2);
     const G = Math.floor(Math.random() * 255 / 2);
     const B = Math.floor(Math.random() * 255 / 2);
 
-    console.log(R, G, B);
-    console.log(`${R.toString(16)}${G.toString(16)}${B.toString(16)}`);
+    setTimeout(() => {
 
+        console.log(R, G, B);
+
+    }, 3000);
+
+    setTimeout(() => {
+
+        console.log(`${R.toString(16)}${G.toString(16)}${B.toString(16)}`);
+
+    }, 5000);
 }
 
 main();
